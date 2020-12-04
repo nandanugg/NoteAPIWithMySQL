866ce46747f06bbd00bf4843869cf8d11a799343
nandanugg <navy.techno@gmail.com>
2020-12-04 16:19:22 +0700

    edit patch note

--- routes/note/editNote.js
+++ routes/note/editNote.js
@@ -3,13 +3,13 @@ const notes = require('../../databases/notesDb')
 const app = express()
 
 // 👇 handle PATCH request method at /note
-app.patch('/note', (req, res) => {
-  // 👇 use req "body" property and use the "body" id property to access id property inserted at body
-  const id = req.body.id
+app.patch('/note/:id', (req, res) => {
+  // 👇 use req "params" property and use id property inserted at path
+  const id = req.params.id
   // 👇 find the index number in notes that has the same id with the id inserted at request by using "findIndex" method
   const editedIndex = notes.findIndex((note) => note.id === id)
   // 👇 access the notes with found index number and change the value based on what inserted in data property at body
-  notes[editedIndex] = req.body.data
+  notes[editedIndex] = req.body
   // 👇 send edited data
   res.send(notes[editedIndex])
 })
