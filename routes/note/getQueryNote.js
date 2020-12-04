a3c287206f366e9b603372a27e70a3df0e43be55
nandanugg <navy.techno@gmail.com>
2020-12-04 16:06:23 +0700

    fix ambiguity

--- routes/note/getQueryNote.js
+++ routes/note/getQueryNote.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/note/query', (req, res) => {
   // 👇 access search property in query and save it to search variable
   const search = req.query.search
-  // 👇 filter notes that includes a same world with the search inserted at request by using "filter" method
+  // 👇 filter notes that includes a same word with the search inserted at request by using "filter" method
   const foundNote = notes.filter((note) => note.note.includes(search))
   res.send(foundNote)
 })
