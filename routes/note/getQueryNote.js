2d3a31343d03ec2d497c94ceac0d31e9ff3de589
nandanugg <navy.techno@gmail.com>
2020-12-04 16:05:05 +0700

    modify note query

--- routes/note/getQueryNote.js
+++ routes/note/getQueryNote.js
@@ -3,10 +3,10 @@ const notes = require('../../databases/notesDb')
 const app = express()
 
 app.get('/note/query', (req, res) => {
-  // 👇 access whatever inserted in query and save it to id variable
-  const id = req.query.id
-  // 👇 filter notes that has the same id with the id inserted at request by using "filter" method
-  const foundNote = notes.filter((note) => note.id === id)
+  // 👇 access search property in query and save it to search variable
+  const search = req.query.search
+  // 👇 filter notes that includes a same world with the search inserted at request by using "filter" method
+  const foundNote = notes.filter((note) => note.note.includes(search))
   res.send(foundNote)
 })
 
