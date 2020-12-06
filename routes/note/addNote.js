9c57a49baea7687d9f2b31f2bbd2ddea31ee3d4c
nandanugg <navy.techno@gmail.com>
2020-12-07 05:03:18 +0700

    Add comment

--- routes/note/addNote.js
+++ routes/note/addNote.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const notes = require('../../databases/notesDb')
 const authorize = require('../../middlewares/authorizationMiddleware')
+// 👇 use nanoid method in nanoid npm module
 const { nanoid } = require('nanoid')
 const app = express()
 
@@ -9,10 +10,15 @@ app.use(authorize)
 
 app.post('/note', (req, res) => {
   const body = req.body
+  // 👇 get user property inserted by authorize middleware
   const user = req.user
+  // 👇 add username property to tell that the note is created by username inside user property
   body.username = user.username
+  // 👇 use nanoid function to create an id
   const id = nanoid()
+  // 👇 add id property with value of created id
   body.id = id
+  // 👇 add note
   notes.push(body)
   res.send(req.body)
 })
