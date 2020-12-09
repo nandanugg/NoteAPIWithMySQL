b196ad33c7915d1375c11874952bb1ec64bddac5
nandanugg <navy.techno@gmail.com>
2020-12-10 02:39:13 +0700

    Fix update command

--- routes/note/editNote.js
+++ routes/note/editNote.js
@@ -6,7 +6,7 @@ const app = express()
 app.patch('/note/:id', async (req, res, next) => {
   const id = req.params.id
   // 👇 update note with anything inside body
-  await db('notes').update({ id }).where(req.body)
+  await db('notes').update(req.body).where({ id })
     .catch((error) => {
       next(error)
     })
